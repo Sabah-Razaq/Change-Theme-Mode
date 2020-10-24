95e2cb73dfdf9e5c1af3e49ef139f793b9eeefc2
Sabah-Razaq <64074203+Sabah-Razaq@users.noreply.github.com>
2020-10-24 19:31:30 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+function darkMode() {
+    var body = document.querySelector('body');
+    var currentClass = body.className;
+    body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+  }
+
+
+
+
+
+
